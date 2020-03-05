a10fa5e4ccac45762032357842cf2b92d6879df7
Sonja Parsell <hello@sparsell.com>
2020-03-05 17:35:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-
+  puts "__ | __ | __"
+  puts "------------"
+  puts "__ | __ | __"
+  puts "------------"
+  puts "__ | __ | __"
 end
